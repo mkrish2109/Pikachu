8217f0d539fef7baf54823bca0b89e1e1ee789f8
mkrish2109 <mkrish1106@gmail.com>
2024-09-10 15:10:24 +0530

    gsap in nav

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import React, { useRef } from "react";
 import { Link } from "react-router-dom";
 
 function Navbar() {
+  const ref = useRef();
+
+  var ti = gsap.timeline();
+
+  useGSAP(() => {
+    ti.from("h1", {
+      y: -100,
+      opacity: 0,
+      duration: 0.8,
+      delay: 0.5,
+    });
+  });
+
+  useGSAP(() => {
+    ti.from("li", {
+      y: -100,
+      opacity: 0,
+      duration: 1,
+      delay: 0.5,
+      stagger: 0.3,
+    });
+  });
+
   const links = [
     { name: "Home", to: "/" },
     { name: "Pokedex", to: "/pokedex" },
@@ -14,12 +39,14 @@ function Navbar() {
           <h1 className="text-xl font-bold tracking-wide">Pokedex</h1>
         </Link>
       </div>
-      <ul className="flex m-0 p-0 list-none gap-4 cursor-pointer">
+      <ul ref={ref} className="flex m-0 p-0 list-none gap-4 cursor-pointer">
         {links.map((value) => {
           return (
-            <li className="list-none text-lg font-semibold hover:text-[#2d3e47]">
-              <Link to={value.to}>{value.name}</Link>
-            </li>
+            <Link to={value.to}>
+              <li className="list-none text-lg font-semibold hover:text-[#2d3e47]">
+                {value.name}
+              </li>
+            </Link>
           );
         })}
       </ul>
